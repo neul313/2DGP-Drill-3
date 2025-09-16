1f1b45be3455a92d56039a76041726158924692e
Jo shinBe <tls3123@tukorea.ac.kr>
2025-09-16 22:45:54 +0900

    삼각형 이동 함수 완료_코파일럿 이용

--- character_moves_copilot.py
+++ character_moves_copilot.py
@@ -34,10 +34,33 @@ def move_circle():
         y = r * math.sin(math.radians(deg)) + cy
         draw_boy(x, y)
 
+def move_triangle():
+    print("move_triangle")
+    # 삼각형 꼭짓점 좌표
+    x1, y1 = 400, 550   # 위쪽 꼭짓점
+    x2, y2 = 750, 50    # 오른쪽 아래 꼭짓점
+    x3, y3 = 50, 50     # 왼쪽 아래 꼭짓점
+    steps = 100
+
+    # 1번 변: 위 -> 오른쪽 아래
+    for i in range(steps + 1):
+        x = x1 + (x2 - x1) * i / steps
+        y = y1 + (y2 - y1) * i / steps
+        draw_boy(x, y)
+    # 2번 변: 오른쪽 아래 -> 왼쪽 아래
+    for i in range(steps + 1):
+        x = x2 + (x3 - x2) * i / steps
+        y = y2 + (y3 - y2) * i / steps
+        draw_boy(x, y)
+    # 3번 변: 왼쪽 아래 -> 위
+    for i in range(steps + 1):
+        x = x3 + (x1 - x3) * i / steps
+        y = y3 + (y1 - y3) * i / steps
+        draw_boy(x, y)
+
 while True:
     move_rectangle()
     move_circle()
-    break
+    move_triangle()
 
 close_canvas()
-
