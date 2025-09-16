1842003e98f6ea65889882464ef8f505c4a6cda1
Jo shinBe <tls3123@tukorea.ac.kr>
2025-09-16 11:10:12 +0900

    원 운동 구현 완료

--- character_moves.py
+++ character_moves.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -13,16 +14,21 @@ def move_rectangle():
 
 def move_circle():
     print("move_circle")
-    clear_canvas_now()
-    boy.draw_now(400,300)
-    delay(0.1)
+    r=200
+    for deg in range(0,360):
+        x= r * math.cos(math.radians(deg)) + 400
+        y= r * math.sin(math.radians(deg)) + 300
+
+        clear_canvas_now()
+        boy.draw_now(x, y)
+        delay(0.1)
     pass
 
 
 while True:
     move_circle()
     move_rectangle()
-    #break
+    break
     pass
 
 
